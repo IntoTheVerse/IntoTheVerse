b8bb935f3989edab5e1fd81cb88d1328a468fde8
lopeselio <lopeselio@gmail.com>
2022-04-15 01:39:28 +0530

    updated components

--- intotheverse/src/pages/Landing/Landing.js
+++ intotheverse/src/pages/Landing/Landing.js
@@ -4,6 +4,8 @@ import Header from '../../components/Metaverse/Header/Header';
 import HomeScreen from '../../components/Metaverse/HomeScreen/HomeScreen';
 import PartnersandInvestors from '../../components/Metaverse/PartnersandInvestors/PartnersandInvestors';
 import Footer from '../../components/Metaverse/Footer/Footer';
+import InfoScreen from '../../components/Metaverse/InfoScreen/InfoScreen'
+import LoreGallery from '../../components/Metaverse/YoutubeSection/LoreGallery';
 // import TeamInfoScreen from '../TeamInfoScreen/TeamInfoScreen';
 // import Footer from '../Footer/Footer';
 // import RoadmapScreen from '../RoadmapScreen/RoadmapScreen';
@@ -13,6 +15,8 @@ const Landing = () => {
         <section className="landing_Container">
             <Header />
             <HomeScreen />
+            <InfoScreen />
+            <LoreGallery />
             <PartnersandInvestors />
             <Footer />
             {/*<FeaturesScreen id="featuresScreen" />
