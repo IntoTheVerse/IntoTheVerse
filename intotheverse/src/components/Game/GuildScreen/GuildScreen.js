ec641ae4a6c2377047742aac36f15e4ee6e743c2
lopeselio <lopeselio@gmail.com>
2022-04-24 23:59:21 +0530

    guild assets

--- intotheverse/src/components/Game/GuildScreen/GuildScreen.js
+++ intotheverse/src/components/Game/GuildScreen/GuildScreen.js
@@ -32,6 +32,12 @@ const GuildScreen = () => {
                     <span className="GuildScreen__titleContainer__highlightText">"</span>
                 </p>
             </section>
+            <p className='oneLiner'>Elders have been trying to master the powers within the mystical objects collected by various warriors over a period of last 450 years and were successful in figuring out a way to transform people into warrior with differnt and unique set of traits. This gave birth to the concept of guild, Veneficus was the first guild. Currenlty they have found 10 guilds and are trying to find more.</p>
+            <section className="GuildScreen__titleContainer">
+                <p>
+                    THREE GUILDS & MORE COMING
+                </p>
+            </section>
             <section className="GuildScreen__widgetContainer">
                 <section className="GuildScreen__widgetContainer__Box">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/jpcm_5E-BiA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
